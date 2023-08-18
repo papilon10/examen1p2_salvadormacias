4ca487107048af14805c58af52d08e2d84538a8b
papilon10 <140213599+papilon10@users.noreply.github.com>
2023-08-18 16:19:28 -0600

    clases finalizadas y agregar jugador

--- src/main/java/com/mycompany/examen1p2_salvador_macias/delantero.java
+++ src/main/java/com/mycompany/examen1p2_salvador_macias/delantero.java
@@ -4,10 +4,124 @@
  */
 package com.mycompany.examen1p2_salvador_macias;
 
+import java.util.Random;
+
 /**
  *
  * @author papilon10
  */
-public class delantero extends jugador{
-    
+public class delantero extends jugador {
+
+    public delantero(String nombre, int edad, String nacionalidad, String pie, equipo equipo) {
+        super(nombre, edad, nacionalidad, pie, equipo);
+    }
+
+    @Override
+    public void agarre() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.agarre = i;
+    }
+
+    @Override
+    public void fisico() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.fisico = i;
+    }
+
+    @Override
+    public void ritmo() {
+        Random rand = new Random();
+        boolean bool = true;
+        while (bool) {
+            int num_rand = rand.nextInt(13);
+            int i = num_rand * 12;
+            if (i >= 70 && i < 100) {
+                this.ritmo = i;
+                bool = false;
+            } else {
+                bool = true;
+            }
+        }
+    }
+
+    @Override
+    public void entrada() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.entrada = i;
+    }
+
+    @Override
+    public void vision() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.vision = i;
+    }
+
+    @Override
+    public void passing() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.passing = i;
+    }
+
+    @Override
+    public void regate() {
+        Random random = new Random();
+        boolean bool = true;
+        while (bool) {
+            int num = random.nextInt(13);
+            int i = num * 12;
+            if (i >= 70 && i < 100) {
+                this.regate = i;
+                bool = false;
+            } else {
+                bool = true;
+            }
+        }
+    }
+
+    @Override
+    public void disparo() {
+        Random rand = new Random();
+        boolean bool = true;
+        while (bool) {
+            int num = rand.nextInt(13);
+            int i = num * 12;
+            if (i >= 70 && i < 100) {
+                this.disparo = i;
+                bool = false;
+            } else {
+                bool = true;
+            }
+        }
+    }
+
+    @Override
+    public void lanzamiento() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int res = num * 13;
+        this.lanzamiento = res;
+    }
+
+    @Override
+    public void rating() {
+        int res = (this.fisico + this.agarre + this.disparo + this.entrada + this.lanzamiento
+                + this.passing + this.ritmo + this.regate + this.vision) / 9;
+        this.rating = res;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
 }
--- src/main/java/com/mycompany/examen1p2_salvador_macias/mediocampista.java
+++ src/main/java/com/mycompany/examen1p2_salvador_macias/mediocampista.java
@@ -4,10 +4,124 @@
  */
 package com.mycompany.examen1p2_salvador_macias;
 
+import java.util.Random;
+
 /**
  *
  * @author papilon10
  */
-public class mediocampista extends jugador{
-    
+public class mediocampista extends jugador {
+
+    public mediocampista(String nombre, int edad, String nacionalidad, String pie, equipo equipo) {
+        super(nombre, edad, nacionalidad, pie, equipo);
+    }
+
+    @Override
+    public void agarre() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.agarre = i;
+    }
+
+    @Override
+    public void fisico() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.fisico = i;
+    }
+
+    @Override
+    public void ritmo() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.ritmo = i;
+    }
+
+    @Override
+    public void entrada() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.entrada = i;
+    }
+
+    @Override
+    public void vision() {
+        Random random = new Random();
+        boolean bool = true;
+        while (bool) {
+            int num = random.nextInt(13);
+            int i = num * 12;
+            if (i >= 70 && i < 100) {
+                this.vision = i;
+                bool = false;
+            } else {
+                bool = true;
+            }
+        }
+    }
+
+    @Override
+    public void passing() {
+        Random random = new Random();
+        boolean bool = true;
+        while (bool) {
+            int num = random.nextInt(13);
+            int i = num * 12;
+            if (i >= 70 && i < 100) {
+                this.passing = i;
+                bool = false;
+            } else {
+                bool = true;
+            }
+        }
+    }
+
+    @Override
+    public void regate() {
+        Random random = new Random();
+        boolean bool = true;
+        while (bool) {
+            int num = random.nextInt(13);
+            int i = num * 12;
+            if (i >= 70 && i < 100) {
+                this.regate = i;
+                bool = false;
+            } else {
+                bool = true;
+            }
+        }
+    }
+
+    @Override
+    public void disparo() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.disparo = i;
+    }
+
+    @Override
+    public void lanzamiento() {
+        Random random = new Random();
+        int num = random.nextInt(6);
+        int i = num * 13;
+        this.lanzamiento = i;
+    }
+
+    @Override
+    public void rating() {
+        int rating = (this.fisico + this.agarre + this.disparo + this.entrada + this.lanzamiento + this.passing
+                + this.ritmo + this.regate + this.vision) / 9;
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
 }
